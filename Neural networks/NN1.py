ee231d1db9c973a5d1502620f1d9df03e8023963
starlordvk <chocoshake2007@gmail.com>
2017-09-21 23:08:59 +0530

    Cost function error

--- Neural networks/NN1.py
+++ Neural networks/NN1.py
@@ -3,6 +3,7 @@ import csv
 import math
 import operator
 import random
+from scipy.special import expit
 
 def sigmoid(z):
 	s=1/(1+np.exp(-z))
@@ -32,7 +33,7 @@ def initialize_paramteres(n_x,n_h,n_y):
 				"b2":b2}
 	return parameters
 
-def forward_propagation	(X,paramters):
+def forward_propagation	(X,parameters):
 	W1=parameters["W1"]
 	b1=parameters["b1"]
 	W2=parameters["W2"]
@@ -41,7 +42,7 @@ def forward_propagation	(X,paramters):
 	Z1=np.dot(W1,X)+b1
 	A1=np.tanh(Z1)
 	Z2=np.dot(W2,A1)+b2
-	A2=sigmoid(Z2)
+	A2=expit(Z2)
 
 	assert(A2.shape==(1,X.shape[1]))
 
@@ -50,7 +51,103 @@ def forward_propagation	(X,paramters):
 			"Z2":Z2,
 			"A2":A2}
 	return A2,cache
+
+def compute_cost(A2,Y,parameters):
+	m=Y.shape[1]
+	print("A2 = "+str(A2))
+	logprobs=(np.multiply(np.log(A2),Y)+np.multiply(np.log(1-A2),1-Y))
+	cost=-np.sum(logprobs)/m
+
+	cost=float(cost)
+	assert(isinstance(cost,float))
+
+	return cost
 						
+
+def backward_propagation(parameters,cache,X,Y):
+	m=X.shape[1]
+	W1=parameters["W1"]
+	W2=parameters["W2"]
+
+	A1=cache["A1"]
+	A2=cache["A2"]
+
+	dZ2=A2-Y
+	dW2=np.dot(dZ2,A1.T)/m
+	db2=np.sum(dZ2,axis=1,keepdims=True)/m
+	dZ1=np.dot(W2.T,dZ2)*(1-np.power(A1,2))
+	dW1=np.dot(dZ1,X.T)/m
+	db1=np.sum(dZ1,axis=1,keepdims=True)/m
+
+	grads={"dW1":dW1,
+		   "db1":db1,
+		   "dW2":dW2,
+		   "db2":db2}	
+	return grads
+
+def update_paramters(parameters,grads,learning_rate):
+	W1=parameters["W1"]
+	b1=parameters["b1"]
+	W2=parameters["W2"]
+	b2=parameters["b2"]
+
+	dW1=grads["dW1"]
+	db1=grads["db1"]
+	dW2=grads["dW2"]
+	db2=grads["db2"]		
+		 
+	W1=W1-learning_rate*dW1
+	b1=b1-learning_rate*db1
+	W2=W2-learning_rate*dW2
+	b2=b2-learning_rate*db2	   	
+
+	parameters={"W1":W1,
+				"b1":b1,
+				"W2":W2,
+				"b2":b2}
+	return parameters
+
+
+def nn_model(X,Y,n_h,num_iterations=1000, print_cost=False):
+	n_x,n_h,n_y=layer_sizes(X,Y)
+	print("size of imput layer is n_x ="+str(n_x))
+	print("size of hidden layer is n_xh ="+str(n_h))
+	print("size of output layer is n_y ="+str(n_y))
+
+	parameters= initialize_paramteres(n_x,n_h,n_y)
+	print("W1 = " + str(parameters["W1"]))
+	print("b1 = " + str(parameters["b1"]))
+	print("W2 = " + str(parameters["W2"]))
+	print("b2 = " + str(parameters["b2"]))
+
+
+	for i in range(0,num_iterations):
+
+		A2,cache=forward_propagation(X,parameters)
+		#print(A2.shape)
+		#print("A2 = "+str(A2))
+		cost =compute_cost(A2,Y,parameters)
+		grads=backward_propagation(parameters,cache,X,Y)
+		parameters=update_paramters(parameters,grads,1.2)
+		if print_cost and i%100==0:
+			print("Cost afetr iteration %i:%f" %(i,cost))
+
+
+	return parameters
+
+	'''
+	print("dW1 = "+str(grads["dW1"]))
+	print("db1 = "+str(grads["db1"]))
+	print("dW2 = "+str(grads["dW2"]))
+	print("db2 = "+str(grads["db2"]))
+
+	
+	print("W1 = " + str(parameters["W1"]))
+	print("b1 = " + str(parameters["b1"]))
+	print("W2 = " + str(parameters["W2"]))
+	print("b2 = " + str(parameters["b2"]))
+	'''
+
 
 
 reader=csv.reader(open("reduced_features.csv","r"),delimiter=",")
@@ -65,20 +162,12 @@ Y=np.array(Y)
 Y=Y.astype(np.int)
 Y=np.transpose(Y)
 
+
 print("shape of x ="+str(X.shape))
 print("shape of y ="+str(Y.shape))
 
-n_x,n_h,n_y=layer_sizes(X,Y)
-print("size of imput layer is n_x ="+str(n_x))
-print("size of hidden layer is n_xh ="+str(n_h))
-print("size of output layer is n_y ="+str(n_y))
-
-parameters= initialize_paramteres(n_x,n_h,n_y)
+parameters = nn_model(X, Y, 4, num_iterations=10, print_cost=False)
 print("W1 = " + str(parameters["W1"]))
 print("b1 = " + str(parameters["b1"]))
 print("W2 = " + str(parameters["W2"]))
-print("b2 = " + str(parameters["b2"]))
-
-A2,cache=forward_propagation(X,parameters)
-print(A2.shape)
-print("A2 = "+str(A2))
+print("b2 = " + str(parameters["b2"]))	
